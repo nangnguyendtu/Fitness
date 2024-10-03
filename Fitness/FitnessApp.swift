10a3a346fd9e7743cd1d6bed7880d85f7efa05f9
Nang Nguyen <nangnguyendtu@gmail.com>
2024-10-03 16:28:06 +0700

    Initial Commit

---
+++ Fitness/FitnessApp.swift
@@ -0,0 +1,17 @@
+//
+//  FitnessApp.swift
+//  Fitness
+//
+//  Created by Mac on 03/10/2024.
+//
+
+import SwiftUI
+
+@main
+struct FitnessApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
